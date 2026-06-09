6e0a6f66ee6ca68d3efb4cabbc7de12d08a755e6
yeang976-art <yeang976@gmail.com>
2026-06-09 11:28:37 +0900

    .

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+void main() {
+    Scanner sc = new Scanner(System.in);
+    String esc = "";
+    System.out.println("[사칙연산 계산기]");
+
+    // Step 1
+    /* while (!esc.equals("exit")) {
+            System.out.println("첫 번째 숫자를 입력하시오");
+            int a = sc.nextInt();
+            System.out.println("'+','-','*','/' 연산기호 중 하나를 입력하시오");
+            char l = sc.next().charAt(0);
+            System.out.println("두 번째 숫자를 입력하시오");
+            int b = sc.nextInt();
+
+            switch (l) {
+                case '+':
+                    System.out.println("결과: " + (a + b));
+                    break;
+                case '-':
+                    System.out.println("결과: " + (a - b));
+                    break;
+                case '*':
+                    System.out.println("결과: " + (a * b));
+                    break;
+                case '/':
+                    if (b == 0) {
+                        System.err.println("결과: 값이 안나옵니다.");
+                        break;
+                    } else {
+                        System.out.println("결과: " + (a / b));
+                        break;
+                    }
+                default:
+                    System.err.println("결과: 흠.. 뭔가 잘못된 것 같습니다.");
+                    break;
+            }
+        sc.nextLine();
+        System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+        esc = sc.nextLine();
+    }*/
+
+    // Step 2
+
+}
+
