4ca913d0afda49c14d1e335c5217d88959579c90
yeang976-art <yeang976@gmail.com>
2026-06-10 16:17:48 +0900

    Step 2 까지 완성

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,55 @@
-public class Calculator {
+import java.util.*;
 
+public class Calculator {
+    /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
+    private ArrayList<Integer> save;
+    private Integer answer;
+
+    public Calculator() {
+        save = new ArrayList<>();
+    }
+
+    public void getSaveList() {
+        System.out.print("저장된 수 현황: ");
+        for (int ea : save) {
+            System.out.print("[" + ea + "], ");
+        }
+    }
+
+    public void setSaveList() {
+        Collections.sort(save);
+    }
+
+    public void calculate(int a, char calc, int b) {
+        answer = null;
+        switch (calc) {
+            case '+':
+                answer = a + b;
+                break;
+            case '-':
+                answer = a - b;
+                break;
+            case '*':
+                answer = a * b;
+                break;
+            case '/':
+                if (b == 0) System.err.println("값이 안나옵니다.");
+                else answer = a / b;
+                break;
+            default:
+                System.err.println("흠.. 뭔가 잘못된 것 같습니다.");
+                break;
+        }
+
+        if (answer != null) save.add(answer);
+   }
+
+    public void removeResult() {
+        save.clear();
+    }
+
+   @Override
+    public String toString() {
+        return "결과: " + answer;
+   }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ void main() {
     System.out.println("[사칙연산 계산기]");
 
     // Step 1
-    /* while (!esc.equals("exit")) {
+    /*
+     while (!esc.equals("exit")) {
             System.out.println("첫 번째 숫자를 입력하시오");
             int a = sc.nextInt();
             System.out.println("'+','-','*','/' 연산기호 중 하나를 입력하시오");
@@ -27,11 +28,10 @@ void main() {
                 case '/':
                     if (b == 0) {
                         System.err.println("결과: 값이 안나옵니다.");
-                        break;
                     } else {
                         System.out.println("결과: " + (a / b));
-                        break;
                     }
+                    break;
                 default:
                     System.err.println("결과: 흠.. 뭔가 잘못된 것 같습니다.");
                     break;
@@ -39,9 +39,29 @@ void main() {
         sc.nextLine();
         System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
         esc = sc.nextLine();
-    }*/
+    }
+    sc.close(); */
 
     // Step 2
+    Calculator calculator1 = new Calculator();
+    while (!esc.equals("exit")) {
+        System.out.println("첫 번째 숫자를 입력하시오");
+        int a = sc.nextInt();
+        System.out.println("'+','-','*','/' 연산기호 중 하나를 입력하시오");
+        char l = sc.next().charAt(0);
+        System.out.println("두 번째 숫자를 입력하시오");
+        int b = sc.nextInt();
 
+        calculator1.calculate(a,l,b);
+        System.out.println(calculator1);
+        calculator1.setSaveList(); // 오름차순 정렬
+        calculator1.getSaveList();
+
+        sc.nextLine();
+        System.out.println("\n더 계산하시겠습니까? (exit 입력 시 종료)");
+        esc = sc.nextLine();
+    }
+    calculator1.removeResult();
+    sc.close();
 }
 
